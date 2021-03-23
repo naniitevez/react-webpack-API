6e744ee24a9e9e54b749f187a54907e76832672e
Nicole Tevez <71675833+naniitevez@users.noreply.github.com>
2021-03-23 14:46:06 -0300

    Config. e instanciacion de React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+const App = () => {
+    return ( 
+        <Fragment>
+            <h1>
+                Hello World!
+            </h1>
+        </Fragment>
+    );
+}
+
+export default App;
